df387fb363d070ee61a2b2311b356d35b5b12e70
象象翔 <32326077178@qqcom>
2026-06-23 15:59:47 +0800

    用户模块后端2 开发 MyBatis Flex 代码生成器 开发核心功能，比如用户注册、用户登录、获取登录用户信息、用户注销等 全局权限校验 AOP 开发管理员增删改查接口 自定义配置解决丢失精度问题

---
+++ src/main/java/com/xiang/xiangaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.xiang.xiangaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.xiang.xiangaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/lyxnbclass">程序员shawn</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
